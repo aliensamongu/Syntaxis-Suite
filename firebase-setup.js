84272f5a3ea63612da42de72a2325c8fd8f414b4
aliensamongu <aliens_amongu@yahoo.com>
2026-05-10 15:00:39 -0400

    Add files via upload

---
+++ firebase-setup.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyAWEDa3YDhaOrYPT4oNrwqEXRk8Jmo8vo8",
+  authDomain: "syntaxis-bible-study-suite.firebaseapp.com",
+  projectId: "syntaxis-bible-study-suite",
+  storageBucket: "syntaxis-bible-study-suite.firebasestorage.app",
+  messagingSenderId: "348178982976",
+  appId: "1:348178982976:web:2b27ba62e26a4655add7a5"
+};
+
+// Initialize Firebase for the whole suite
+const app = initializeApp(firebaseConfig);
